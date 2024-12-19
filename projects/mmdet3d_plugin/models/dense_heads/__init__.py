7afb8b30e747a441cb4eed554497539f84e659df
rasd3 <yckim@spa.hanyang.ac.kr>
2024-12-19 01:53:38 +0000

    [add] split-and-fusion branch

--- projects/mmdet3d_plugin/models/dense_heads/__init__.py
+++ projects/mmdet3d_plugin/models/dense_heads/__init__.py
@@ -2,6 +2,7 @@ from .uni3detr_head import Uni3DETRHead
 from .uni3detr_head_clip import Uni3DETRHeadCLIP
 from .uni3detr_head_clip_cls_agn import Uni3DETRHeadCLIPClsAgn
 from .uni3detr_head_clip_dn import Uni3DETRHeadCLIPDN
+from .uni3detr_head_clip_dn_saf import Uni3DETRHeadCLIPDNSAF
 
 __all__ = ['Uni3DETRHead', 'Uni3DETRHeadCLIP', 'Uni3DETRHeadCLIPClsAgn',
-           'Uni3DETRHeadCLIPDN']
+           'Uni3DETRHeadCLIPDN', 'Uni3DETRHeadCLIPDNSAF']
